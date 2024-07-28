45c1578bbc6618babc0a768d01e81bb0c27a4ed7
zeel9739 <160000671+zeel9739@users.noreply.github.com>
2024-07-28 15:59:52 +0530

    Update authorization.js

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,16 +1,45 @@
-const jwt = require("jsonwebtoken");
-const config = require("../utils/auth.config");
-module.exports = async (req, res, next) => {
-  try {
-    const jwtToken = req.header("token");
-    if (!jwtToken) {
-      return res.json({ message: "Not Authorize." });
-    }
-    const payload = jwt.verify(jwtToken, config.secret);
-    req.user = payload.user;
-  } catch (err) {
-    return res.json(err.message);
-  }
+// const jwt = require("jsonwebtoken");
+// const config = require("../utils/auth.config");
+// module.exports = async (req, res, next) => {
+//   try {
+//     const jwtToken = req.header("token");
+//     if (!jwtToken) {
+//       return res.json({ message: "Not Authorize." });
+//     }
+//     const payload = jwt.verify(jwtToken, config.secret);
+//     req.user = payload.user;
+//   } catch (err) {
+//     return res.json(err.message);
+//   }
 
-  next();
+//   next();
+// };
+
+const jwt = require("jsonwebtoken");
+const NodeCache = require('node-cache');
+const cache = new NodeCache();
+const config = require("../utils/auth.config");
+
+module.exports = async (req, res, next) => {
+    try {
+        const jwtToken = req.header("token");
+        if (!jwtToken) {
+            return res.json({ message: "Not Authorize." });
+        }
+
+        const cachedUser = cache.get(jwtToken);
+        if (cachedUser) {
+            req.user = cachedUser;
+            return next();
+        }
+
+        const payload = jwt.verify(jwtToken, config.secret);
+        req.user = payload.user;
+        cache.set(jwtToken, payload.user, 3600); // Cache for 1 hour
+    } catch (err) {
+        return res.json(err.message);
+    }
+
+    next();
 };
+
